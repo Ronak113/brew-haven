785544668c55df58fa0fdb102e742d40fcd3c922
Ronak Vasoya <ronakvasoya289@gmail.com>
2026-02-13 20:56:39 +0530

    Fix dns

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,35 +1,37 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const dns = require("node:dns");
+
 dns.setDefaultResultOrder("ipv4first");
 
-
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 587,
-  secure: false, // true for 465, false for other ports
+  secure: false, // true only for 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  family: 4
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 // Verify transporter once (on server start)
 transporter.verify((error, success) => {
-    if (error) {
-        console.error('❌ Email transporter error:', error);
-    } else {
-        console.log('✅ Email server is ready to send messages');
-    }
+  if (error) {
+    console.error("❌ Email transporter error:", error);
+  } else {
+    console.log("✅ Email server is ready to send messages");
+  }
 });
 
-const sendEmail = async ({to, subject, html}) => {
-    await transporter.sendMail({
-        from: `"Brew Haven ☕"<${process.env.EMAIL_USER}>`,
-        to,
-        subject,
-        html,
-    });
+const sendEmail = async ({ to, subject, html }) => {
+  await transporter.sendMail({
+    from: `"Brew Haven ☕" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
 };
 
 module.exports = sendEmail;
