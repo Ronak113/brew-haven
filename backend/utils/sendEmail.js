7e7debaa77f625fe849822d76d5959b7bd125e8c
Ronak Vasoya <ronakvasoya289@gmail.com>
2026-02-13 20:36:10 +0530

    Fix smtp

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -8,6 +8,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  family: 4
 });
 
 // Verify transporter once (on server start)
