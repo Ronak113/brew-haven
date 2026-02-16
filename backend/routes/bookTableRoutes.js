f5a7d7a9f2fd9689601cb226ab1ba5de4c672aba
Ronak Vasoya <ronakvasoya289@gmail.com>
2026-02-16 18:30:31 +0530

    add gmail system

--- backend/routes/bookTableRoutes.js
+++ backend/routes/bookTableRoutes.js
@@ -1,28 +1,27 @@
-import express from 'express'
-const router = express.Router();
-import db from '../config/db.js'
+import express from "express";
+import db from "../config/db.js";
 import sendEmail from "../utils/sendEmail.js";
 
+const router = express.Router();
 
-router.post('/', async (req, res) => {
-    const { name, email, phone, date, time, guests } = req.body;
+router.post("/", async (req, res) => {
+  const { name, email, phone, date, time, guests } = req.body;
 
-    // Basic validation
-    if (!name || !email || !phone || !date || !time || !guests) {
-        return res.status(400).json({ error: 'All fields are required' });
-    }
+  if (!name || !email || !phone || !date || !time || !guests) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
 
-  const sql = 'INSERT INTO book_table (name, email, phone, date, time, guests) VALUES (?, ?, ?, ?, ?, ?)';
+  const sql =
+    "INSERT INTO book_table (name, email, phone, date, time, guests) VALUES (?, ?, ?, ?, ?, ?)";
 
-  db.query(sql, [name, email, phone, date, time, guests], async (err, result) => {
+  db.query(sql, [name, email, phone, date, time, guests], async (err) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: 'Database error' });
+      return res.status(500).json({ error: "Database error" });
     }
 
-    //USER EMAIL
-    await sendEmail.sendMail({
-       from: `"Brew Haven" <${process.env.EMAIL_USER}>`,
+    // USER EMAIL
+    await sendEmail({
       to: email,
       subject: "Reservation Confirmed 🍽️",
       html: `
@@ -32,13 +31,12 @@ router.post('/', async (req, res) => {
         <p><b>Time:</b> ${time}</p>
         <p><b>Guests:</b> ${guests}</p>
         <br/>
-        <p>Thank you for choosing Brew Haven ☕</p> 
-        `
+        <p>Thank you for choosing Brew Haven ☕</p>
+      `,
     });
 
-     // ADMIN EMAIL
-    await sendEmail.sendMail({
-      from: `"Brew Haven" <${process.env.EMAIL_USER}>`,
+    // ADMIN EMAIL
+    await sendEmail({
       to: process.env.ADMIN_EMAIL,
       subject: "New Table Reservation 🚨",
       html: `
@@ -49,13 +47,11 @@ router.post('/', async (req, res) => {
         <p><b>Date:</b> ${date}</p>
         <p><b>Time:</b> ${time}</p>
         <p><b>Guests:</b> ${guests}</p>
-      `
+      `,
     });
 
-    res.status(201).json({ message: 'Booking successful' });
-    });
-
-    } 
-);
+    res.status(201).json({ message: "Booking successful & email sent" });
+  });
+});
 
 export default router;
