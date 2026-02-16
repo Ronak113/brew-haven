949e0a5a61609b3283fda6470f2d7ef41b490845
Ronak Vasoya <ronakvasoya289@gmail.com>
2026-02-16 20:48:07 +0530

    add gmail system

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,22 @@ app.use('/api/contact', contactRoutes);
 import bookTableRoutes from './routes/bookTableRoutes.js'
 app.use('/api/book-table', bookTableRoutes);
 
+import sendEmail from './utils/sendEmail.js';
+app.get("/test-email", async (req, res) => {
+  try {
+    await sendEmail({
+      from: process.env.EMAIL_FROM,
+      to: process.env.ADMIN_EMAIL,
+      subject: "SMTP Test",
+      html: "<h2>Email system working 🎉</h2>",
+    });
+
+    res.send("Email sent");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err.message);
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 
