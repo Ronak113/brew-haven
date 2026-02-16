cf4384403489f410b70956820ff69521c9860396
Ronak Vasoya <ronakvasoya289@gmail.com>
2026-02-16 17:33:41 +0530

    add gmail system

--- backend/routes/bookTableRoutes.js
+++ backend/routes/bookTableRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
-
+import { transporter } from '../utils/sendEmail';
 
 
 
@@ -15,11 +15,43 @@ router.post('/', async (req, res) => {
 
   const sql = 'INSERT INTO book_table (name, email, phone, date, time, guests) VALUES (?, ?, ?, ?, ?, ?)';
 
-  db.query(sql, [name, email, phone, date, time, guests], (err, result) => {
+  db.query(sql, [name, email, phone, date, time, guests], async (err, result) => {
     if (err) {
       console.error(err);
       return res.status(500).json({ error: 'Database error' });
     }
+
+    //USER EMAIL
+    await transporter.sendMail({
+       from: `"Brew Haven" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "Reservation Confirmed 🍽️",
+      html: `
+        <h3>Hello ${name},</h3>
+        <p>Your table is reserved.</p>
+        <p><b>Date:</b> ${date}</p>
+        <p><b>Time:</b> ${time}</p>
+        <p><b>Guests:</b> ${guests}</p>
+        <br/>
+        <p>Thank you for choosing Brew Haven ☕</p> 
+        `
+    });
+
+     // ADMIN EMAIL
+    await transporter.sendMail({
+      from: `"Brew Haven" <${process.env.EMAIL_USER}>`,
+      to: process.env.ADMIN_EMAIL,
+      subject: "New Table Reservation 🚨",
+      html: `
+        <h3>New Booking Received</h3>
+        <p><b>Name:</b> ${name}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Phone:</b> ${phone}</p>
+        <p><b>Date:</b> ${date}</p>
+        <p><b>Time:</b> ${time}</p>
+        <p><b>Guests:</b> ${guests}</p>
+      `
+    });
 
     res.status(201).json({ message: 'Booking successful' });
     });
