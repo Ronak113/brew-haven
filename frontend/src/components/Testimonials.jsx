3e64a582fa8d436a031be80563a30d3640246a36
Ronak Vasoya <ronakvasoya289@gmail.com>
2026-02-18 17:40:53 +0530

    add for ellamia

--- frontend/src/components/Testimonials.jsx
+++ frontend/src/components/Testimonials.jsx
@@ -52,26 +52,15 @@ const Testimonials = () => {
     pauseOnFocus: true,
     responsive: [
       {
-        breakpoint: 10000,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-        },
-      },
-      {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 2,
         },
       },
       {
         breakpoint: 640,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
         },
       },
     ],
